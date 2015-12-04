928af237f58e190ec786721f4dba30d5d437b8f5
Jon Wood <woodjon@gmail.com>
2015-12-04 14:05:40 +0000

    Update index page, add scripts and css, add angular

---
+++ client/app/app.js
@@ -0,0 +1,5 @@
+var app = angular.module("app", []);
+
+app.controller('viddlerController', function($scope) {
+    $scope.myVal = "Welcome to Angular!";
+});
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require("express");
 
-var env = process.env.NODE_ENV = process.env.NODE_ENV | "dev";
-
 var app = express();
 
 app.set("views", __dirname + "/server/views");
 app.set("view engine", "jade");
+
+app.use(express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname + '/client'));
 
 app.get("*", function (req, res) {
     res.render("index");
 });
 
 app.listen(process.env.PORT);
-console.log("Listening on port " + process.env.PORT);
